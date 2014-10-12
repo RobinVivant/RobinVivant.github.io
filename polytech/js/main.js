03b44af4dd8ac5bb7054991b8b6d80680d7a4e0c
RobinVivant <robin.vivant@gmail.com>
2014-10-12 19:23:50 +0200

    update chord code

--- polytech/js/main.js
+++ polytech/js/main.js
@@ -10,7 +10,7 @@ code.getSession().setMode("ace/mode/javascript");
 
 
 $("#example-exec").on('click', function(){
-	eval(code.getValue());
+	//eval(code.getValue());
 	eval(editor.getValue());
 });
 
@@ -21,3 +21,45 @@ $("#example-clear").on('click', function(){
 function output(str){
 	$("#output").append("<p>"+str+"</p>");
 }
+
+
+function Chord(){
+
+	var nodeList = [];
+	
+	function createNode( nodeId ){
+		var node = new Node(nodeId);
+		nodeList.push(node);
+		
+	}
+	
+	function getNode( index ){
+		return nodeList[i];
+	}
+	
+	//public API
+	this.createNode = createNode;
+	this.getNode = getNode;
+}
+
+function Node( nodeId ){
+	var nodeKey = new ChordKey(nodeId);
+	var predecessor;
+	var successor;
+	var fingerTable = new FingerTable(this)
+	
+	
+	create();
+		
+
+}
+
+function ChordKey( nodeId ){
+	
+}
+
+function FingerTable( node ){
+	
+}
+
+
